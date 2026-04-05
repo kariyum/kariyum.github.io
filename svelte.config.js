165fdc961cbd543f4b419eb99fd5039678f6820e
Karim Ben Amara <karimbenamara91@gmail.com>
2026-04-06 00:14:33 +0100

    handle unseen routes with warn instead of fail temporarily

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
-const dev = process.argv.includes("dev");
+import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -8,8 +8,11 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? "" : process.env.BASE_PATH,
+			base: dev ? '' : process.env.BASE_PATH
 		},
+		prerender: {
+			handleUnseenRoutes: 'warn'
+		}
 	}
 };
 
